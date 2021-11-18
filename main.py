7bfde98de1a127525938f26efffdef9864cad505
DESKTOP-MH1H4PQ\Admin <svasylenko@gloriumtech.com>
2021-11-18 22:39:43 +0200

    first turn

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# task 1
+total = 44
+rez = int(round(44 * 0.8,0))
+print ("Task1")
+print("Для успешного окончания курса надо сдать {} домашек".format(rez))
+assert rez == 35
 
+# task 2
+x1 = round(random.random(),2)
+x2 = round(random.random(),2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+y1 = round(random.random(),2)
+y2 = round(random.random(),2)
 
+dist = round(((x2 - x1)**2 + (y2 - y1)**2)**0.5,4)
+print ("Task2")
+print("A:", x1, y1, "B is:", x2,y2,"\t", "Distance is:", dist)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# task 3
+width = round(random.random(),2)*100
+height = round(random.random(),2)*100
+depth = round(random.random(),2)*100
+diag_sq = round(depth*(width**2 + height**2)**0.5,3)
+if diag_sq%12 == 0:
+    needed = diag_sq/12
+else:
+    needed = diag_sq //12 + 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Task 3")
+
+print("Width:", width, "Height is:", height, "Depth is:", depth,  sep="  ")
+print("Diag_sq:", diag_sq, "Needed:", needed, "Price is:", needed*600, "Remains m2:", diag_sq%12 , sep="  ")
