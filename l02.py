8fca1428c0e14bc094df15a24bb6349e78a5ba65
DESKTOP-MH1H4PQ\Admin <svasylenko@gloriumtech.com>
2021-11-22 23:27:30 +0200

    with disc

--- l02.py
+++ l02.py
@@ -53,22 +53,61 @@
 
 
 # here to be anoter task
+ans_dic = {
+    "layer10000":"здравствуй, если хочешь узнать акции нажми 1, если тариф 2, если погоду 3",
+    "layer12000":"узнать свой тариф 1, новые тарифы 2, остаток по счету 3, вернуться 9",
+    "layer12300":"остаток по основному счету 1 остаток по бонусному 2, вернуться 9",
+    "layer12310":"остаток в гривнах 1 остаток в минутах/гигабайтах 2, вернуться 9",
+    "layer12311":"на вашем счету 5 грн, , вернуться 9"
+}
 
+l1,l2,l3,step=1,0,0,0
+
+def prnt(res):
+    print(ans_dic[res])
+    return str(input("answer:"))
+
+print(ans_dic["layer100"])
+answer = str(input("Choice: "))
+
+
+while answer != 'q':
+    if answer == '9' and l1 == 1: break
+
+    if l2 == 0 and l3 == 0 and answer != '9' and l1 != 1:
+        l2 = int(answer)
+        step = 1
+    if l2 == 0 and l3 == 0 and answer != '9' and step ==0:
+        l1 = int(answer)
+        step = 1
+    if l2 !=0 and l3 == 0 and answer != '9' and step ==0:
+        l3 = int(answer)
+        step =1
+    if l3 != 0 and answer == '9':
+        l3 = 0
+        answer = ''
+    if l3 == 0 and answer == '9':
+        l2 = 0
+        answer = ''
+    if l3 == 0 and l2 == 0 and answer == '9': l1 = l1 - 1
+    step = 0
+    res = "layer" + str(l1) + str(l2) + str(l3)
+    answer = prnt(res)
 
 # here
 
-x = int(input("Enter x:"))
-a = int(input("Enter a:"))
-
-b = (x**2 + 4*a**2 + 4*a*x)/(8*a**3)
-y = (2 + x/a)**3/b
-
-v1 = 8*(2*a+x)
-v2 = 8/(x+2*a)
-v3 = (x+2*a)/8
-v4 = 1/(8*(x+2*a))
-
-if y == v1: print("Answer is 1")
-if y == v2: print("Answer is 2")
-if y == v3: print("Answer is 3")
-if y == v4: print("Answer is 4")
+# x = int(input("Enter x:"))
+# a = int(input("Enter a:"))
+#
+# b = (x**2 + 4*a**2 + 4*a*x)/(8*a**3)
+# y = (2 + x/a)**3/b
+#
+# v1 = 8*(2*a+x)
+# v2 = 8/(x+2*a)
+# v3 = (x+2*a)/8
+# v4 = 1/(8*(x+2*a))
+#
+# if y == v1: print("Answer is 1")
+# if y == v2: print("Answer is 2")
+# if y == v3: print("Answer is 3")
+# if y == v4: print("Answer is 4")
