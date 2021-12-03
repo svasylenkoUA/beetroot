c53c91f31dd7b1c03c7324e5e1161b819f4f1556
DESKTOP-MH1H4PQ\Admin <svasylenko@gloriumtech.com>
2021-12-04 00:41:55 +0200

    functions_additional_tasks

--- HW_functions.py
+++ HW_functions.py
@@ -45,3 +45,91 @@ def make_operation(oper, *args):
 assert make_operation("+", 7, 7, 2) == 16
 assert make_operation("-", 5, 5, -10, -20) == 20
 assert make_operation("*", 7, 6) == 42
+
+
+#Напишите функцию которая будет переводить возраст с Земного на Марсианский. В году на Земле 365 дней а на марсе 687
+
+def mars_age(age: int) -> int:
+    return age*365//687
+
+
+assert mars_age(10) == 5
+assert mars_age(63) == 33
+assert mars_age(1000) == 531
+
+#Напишите функцию greet принимающую имя name (type:str) м слово word (type:str). Если слово не передано то считать его " -" и возвращающую строку вида "[Имя] ты сегодня [слово]!"
+
+def greet(name, word="-"):
+
+    return name.capitalize() + " ты сегодня " + word +"!"
+
+assert greet("111", "2") == "111 ты сегодня 2!"
+assert greet("игорь", "молодец") == "Игорь ты сегодня молодец!"
+assert greet(name="ольга", word="супер") == "Ольга ты сегодня супер!"
+assert greet("николай") == "Николай ты сегодня -!"
+
+
+#Напишите функцию joinA которая принимает неограниченное количество значений любого типа и возвращает строку где эти значения склеены через символ A
+
+#Попробуйте написать с помощью компрехеншена одной строкой return ___magic___
+
+def joinA(*args):
+    return 'A'.join([str(i) for i in args])
+
+assert joinA("Привет", "мир", "!") == "ПриветAмирA!"
+assert joinA("Привет", 1, 2, 3, True) == "ПриветA1A2A3ATrue"
+assert joinA() == ""
+
+# 21
+import random
+
+def get_deck() -> list[dict]:
+    '''Возвращает лист карт'''
+    pass
+
+def shuffle_deck(deck: list[dict]) -> list[dict]:
+    for i in range(1,14):
+        p1, p2 = random.randint(0,12), random.randint(0,12)
+        deck[p1], deck[p2] = deck[p2], deck[p1]
+    return deck
+
+def calculate(hand: list[dict]) -> int:
+    i=res=0
+    while i<len(hand):
+        res += hand[i]
+        i +=1
+    return res
+
+def want_more() -> bool:
+    return his_deck.append(deck.pop(0))
+
+
+
+deck = [2,3,4,5,6,7,8,9,10,10,10,10,11]
+his_deck = []
+my_deck = []
+my_bank = 0
+shuffle_deck(deck)
+
+while True:
+    while input("Want more? ") == "Y":
+        want_more()
+
+    if calculate(his_deck)>21:
+        print("His deck", his_deck)
+        print("Looser!")
+        his_deck.clear()
+        continue
+
+    while my_bank<15:
+        my_bank += deck[0]
+        my_deck.append(deck.pop(0))
+
+    print("My deck", my_deck)
+    print("His deck", his_deck)
+
+    if calculate(my_deck)>21 or calculate(his_deck) > calculate(my_deck):
+        print("You win!")
+    elif calculate(his_deck) == calculate(my_deck):
+        print("Ups, one more time")
+    else: print("I win!")
