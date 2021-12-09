b431d37f784843ba0d0fb8dcfdffbb2fdd0fd935
DESKTOP-MH1H4PQ\Admin <svasylenko@gloriumtech.com>
2021-12-09 22:05:37 +0200

    smt2

--- HW_with_stars.py
+++ HW_with_stars.py
@@ -7,10 +7,11 @@ except:
     print("Dir exists")
 
 os.chdir("cache")
-print(os.getcwd())
+pth = os.getcwd()
 
 for i in range(0,5):
-    f = tempfile.NamedTemporaryFile(delete=False, suffix=".bak")
+    f = tempfile.NamedTemporaryFile(delete=False, suffix=".bak", dir=pth)
+
     with open(f.name, mode="a",encoding="utf-8") as file:
         file.write(" \n")
 
