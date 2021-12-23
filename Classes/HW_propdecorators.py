7b03877c0bd13f65cb8335a48795dc1476fcd59c
DESKTOP-MH1H4PQ\Admin <svasylenko@gloriumtech.com>
2021-12-23 23:15:23 +0200

    HW_classes12

---
+++ Classes/HW_propdecorators.py
@@ -0,0 +1,22 @@
+# import re
+#
+# regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+#
+# class Email_val:
+#
+#     @classmethod
+#     def validate(cls, email):
+#         if (re.fullmatch(regex, email)):
+#             print("Valid Email")
+#         else:
+#             print("Invalid Email")
+#
+# e = Email_val
+# email = "ankitrai326@gmail.com"
+# e.validate(email)
+# email = "my.ownsite@our-earth.org"
+# e.validate(email)
+# email = "ankitrai326.com"
+# e.validate(email)
+
+
