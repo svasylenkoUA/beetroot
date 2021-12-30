aefc23b4d6528df081b151b73602f7d3c337e156
DESKTOP-MH1H4PQ\Admin <svasylenko@gloriumtech.com>
2021-12-30 19:46:06 +0200

    HW_iter3

--- Classes/HW_gen_iter.py
+++ Classes/HW_gen_iter.py
@@ -18,16 +18,48 @@
 #Create your own implementation of a built-in function range, named in_range(), which takes three
 # parameters: `start`, `end`, and optional step. Tips: See the documentation for `range` function
 
-def in_range(start=0, end=0, step=1):
-    cnt = start
-    if step>0:
-        while cnt <= end:
-            yield cnt
-            cnt += step
-    else:
-        while cnt >= end:
-            yield cnt
-            cnt += step
+# def in_range(start=0, end=0, step=1):
+#     cnt = start
+#     if step>0:
+#         while cnt <= end:
+#             yield cnt
+#             cnt += step
+#     else:
+#         while cnt >= end:
+#             yield cnt
+#             cnt += step
+#
+# for m in in_range(11,2, -2):
+#     print(m)
 
-for m in in_range(11,2, -2):
-    print(m)
+
+class New_Iter:
+
+
+    def __init__(self, args, nums):
+        self.__idx = 0
+        self.__data = args
+        self.__nums = nums
+
+    def __iter__(self):
+        self.__idx = 0
+        return self
+
+    def __next__(self):
+        if self.__idx >= len(self.__data):
+            self.__nums -= 1
+            if self.__nums == 0:
+                raise StopIteration
+            self.__idx = 0
+
+        dt = self.__data[self.__idx]
+        self.__idx += 1
+        return dt
+
+    def __getitem__(self, item):
+        return self.__data[item]
+
+l = New_Iter("ABC", 3)
+
+for i in l:
+    print(i)
